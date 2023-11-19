959bd592f62ccedd703fc938957670595edb7bd6
sppratt <sppratt@umich.edu>
2023-11-19 12:53:20 -0800

    starter fils

---
+++ API_3_gather_data.py
@@ -0,0 +1,9 @@
+# Names: Sami Pratt, Natalie Anderson, Mia Trotter
+# Student IDs: 97430564, 
+# Emails: sppratt@umich.edu, nateand@umich.edu, miatrot@umich.edu
+# List any AI tool (e.g. ChatGPT, GitHub Copilot): ChatGPT
+
+# Final Project
+
+# API #3: write at least one function in this file to gather data from the 
+# APIs/website (using Beautiful Soup) and store it in a database
