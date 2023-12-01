f70c9e3a99a35c6d1e5e9a0340080d59b9cebeda
miatrot <miatrot@umich.edu>
2023-12-01 16:25:28 -0500

    api key added

--- population_gather_data.py
+++ population_gather_data.py
@@ -7,3 +7,14 @@
 
 # API #3: write at least one function in this file to gather data from the 
 # APIs/website (using Beautiful Soup) and store it in a database
+
+
+import requests
+
+name = 'San Francisco'
+api_url = 'https://api.api-ninjas.com/v1/city?name={}'.format(name)
+response = requests.get(api_url, headers={'X-Api-Key': 'M5VM2aS8wMwsYdOCutO3dQ==76LK9aFKvZksa0IC'})
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
