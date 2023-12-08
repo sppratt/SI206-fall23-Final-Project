801c9f5f645f0c67cf8d02a9488872b45c2e7288
sppratt <sppratt@umich.edu>
2023-12-08 11:26:25 -0500

    outlining calcs and visuals

--- visualizations.py
+++ visualizations.py
@@ -11,3 +11,50 @@
 #plot temp vs quality of life (cross weather and quality databases)
 #temp vs rank (city id)
 #population vs. quality of life, safety, traffic, cost
+
+# quality of life index per capita by dividing the quality of life by population (join quality and population)
+# can do that with all of the indexes in quality table
+
+def calc1():
+    # do something
+    return
+
+def calc2():
+    # do something
+    return
+
+
+def calc3():
+    # do something
+    return
+
+
+def calculations():
+
+    # list1 = calc #1
+    # list2 = calc #2
+    # list3 = calc #3...
+
+    # write to text file
+    pass
+
+def vis1():
+    # do something
+    pass
+
+def vis2():
+    # do something
+    pass
+
+
+def vis3():
+    # do something
+    pass
+
+def visualizations():
+    pass
+
+def main():
+    calculations()
+    visualizations()
+
