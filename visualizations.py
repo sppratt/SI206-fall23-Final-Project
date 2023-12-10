04a0b68009dff9a63dc65190ff54abeb6e676a89
sppratt <sppratt@umich.edu>
2023-12-10 17:38:39 -0500

    created text file

--- visualizations.py
+++ visualizations.py
@@ -23,6 +23,7 @@ import os
 import matplotlib.pyplot as plt
 import sqlite3
 import pandas as pd
+import numpy as np
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
@@ -57,17 +58,31 @@ def calc3(cur, conn):
     
 
 def calculations(cur, conn):
-    calc1(cur, conn)
-    calc2(cur, conn)
-    calc3(cur, conn)
+    list1 = calc1(cur, conn)
+    # print(list1)
+    list2 = calc2(cur, conn)
+    # print(list2)
+    avg_safety, avg_safety10 = calc3(cur, conn)
 
-    # list1 = calc #1
-    # list2 = calc #2
-    # list3 = calc #3...
+    with open('calculation_results.txt', 'w') as f:
+        f.write('Quality of Life Index per Capita\n\n')
+        f.write("City: Quality of Life Index per Capita\n")
+        for line in list1:
+            f.write(f"{line[0]}: {line[1]}\n")
 
-    # write to text file
-    pass
+        f.write("\n-----------------------------\n\n")
+        f.write("Average Temperature per Country\n\n")
+        f.write("Country: Average Temperature ºF\n")
+        for line in list2:
+            f.write(f"{line[0]}: {line[1]}\n")
+        f.write("\n-----------------------------\n\n")
+        f.write("Average Safety Index of All Cities vs. Average Safety Index of Ten Most Populated Cities\n\n")
+        f.write(f"All Cities: {avg_safety}\nTen Most Populated: {avg_safety10}")
 
+
+
+
+    
 def vis1(cur, conn):
     cur.execute("SELECT cities.city_name, population.latitude, population.longitude FROM population JOIN cities ON cities.city_id = population.city_id;")
     results = cur.fetchall()
@@ -83,47 +98,60 @@ def vis1(cur, conn):
 
     fig.show()
 
-        
-
 def vis2(cur, conn):
     cur.execute("SELECT population.city_id, population.population, quality.quality, quality.safety, quality.traffic, quality.cost FROM population JOIN quality ON quality.city_id = population.city_id")
     results = cur.fetchall()
-    df = pd.DataFrame(results, columns=['City_id', 'Population', 'Quality of Life', 'Safety', 'Traffic', 'Cost'])
-    
+    df = pd.DataFrame(results, columns=['City_id', 'Population', 'Quality of Life', 'Safety', 'Traffic Commute Time', 'Cost of Living'])
+
     fig = make_subplots(rows=2, cols=2,
-                    subplot_titles=['Quality of Life', 'Safety', 'Traffic', 'Cost'],
-                    shared_xaxes=True, shared_yaxes=False,
+                    subplot_titles=['Quality of Life', 'Safety', 'Traffic Commute Time', 'Cost of Living'],
+                    shared_xaxes=False, shared_yaxes=False,
                     horizontal_spacing=0.1, vertical_spacing=0.2)
 
-    # Add scatter plots with trend lines to subplots
     fig.add_trace(go.Scatter(x=df['Population'], y=df['Quality of Life'], mode='markers', name='Quality of Life'),
                 row=1, col=1)
 
-    fig.add_trace(go.Scatter(x=df['Population'], y=df['Quality of Life'].rolling(window=5).mean(), mode='lines', name='Trendline'),
+    fig.add_trace(go.Scatter(x=df['Population'], y=np.polyval(np.polyfit(df['Population'], df['Quality of Life'], 1), df['Population']),
+                mode='lines', name='Trendline'),
                 row=1, col=1)
 
     fig.add_trace(go.Scatter(x=df['Population'], y=df['Safety'], mode='markers', name='Safety'),
                 row=1, col=2)
 
-    fig.add_trace(go.Scatter(x=df['Population'], y=df['Safety'].rolling(window=5).mean(), mode='lines', name='Trendline'),
+    fig.add_trace(go.Scatter(x=df['Population'], y=np.polyval(np.polyfit(df['Population'], df['Safety'], 1), df['Population']),
+                mode='lines', name='Trendline'),
                 row=1, col=2)
 
-    fig.add_trace(go.Scatter(x=df['Population'], y=df['Traffic'], mode='markers', name='Traffic'),
+    fig.add_trace(go.Scatter(x=df['Population'], y=df['Traffic Commute Time'], mode='markers', name='Traffic Commute Time'),
                 row=2, col=1)
 
-    fig.add_trace(go.Scatter(x=df['Population'], y=df['Traffic'].rolling(window=5).mean(), mode='lines', name='Trendline'),
+    fig.add_trace(go.Scatter(x=df['Population'], y=np.polyval(np.polyfit(df['Population'], df['Traffic Commute Time'], 1), df['Population']),
+                mode='lines', name='Trendline'),
                 row=2, col=1)
 
-    fig.add_trace(go.Scatter(x=df['Population'], y=df['Cost'], mode='markers', name='Cost'),
+    fig.add_trace(go.Scatter(x=df['Population'], y=df['Cost of Living'], mode='markers', name='Cost of Living'),
                 row=2, col=2)
 
-    fig.add_trace(go.Scatter(x=df['Population'], y=df['Cost'].rolling(window=5).mean(), mode='lines', name='Trendline'),
+    fig.add_trace(go.Scatter(x=df['Population'], y=np.polyval(np.polyfit(df['Population'], df['Cost of Living'], 1), df['Population']),
+                mode='lines', name='Trendline'),
                 row=2, col=2)
 
-    # Update layout for better presentation
-    fig.update_layout(title_text='Population vs. Quality of Life Factors')
+    fig.update_layout(
+        title_text='Population vs. Quality of Life Factors',
+    )
 
-    # Show the plot
+    fig.update_xaxes(title_text='Population', row=1, col=1)
+    fig.update_yaxes(title_text='Quality of Life Index', row=1, col=1)
+
+    fig.update_xaxes(title_text='Population', row=1, col=2)
+    fig.update_yaxes(title_text='Safety Index', row=1, col=2)
+
+    fig.update_xaxes(title_text='Population', row=2, col=1)
+    fig.update_yaxes(title_text='Traffic Commute Time Index', row=2, col=1)
+
+    fig.update_xaxes(title_text='Population', row=2, col=2)
+    fig.update_yaxes(title_text='Cost of Living Index', row=2, col=2)
+
     fig.show()
 
 
